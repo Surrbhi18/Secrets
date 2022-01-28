bd7558a2af2cb6d81c82e2751580ffa886883eec
Surrbhi18 <surbhig276@gmail.com>
2022-01-28 13:32:41 +0530

    Add Environment Vars

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-//require('dotenv').config();
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -16,6 +16,7 @@ const ejs = require("ejs");
 const app = express();
 const port = 3000
 
+console.log(process.env.API_KEY)
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
